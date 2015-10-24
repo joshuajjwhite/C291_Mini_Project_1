3a64442d2536117c34e41277af1d15d58b629a95
joshuajjwhite <joshua.jj.white@gmail.com>
2015-10-23 23:26:31 -0600

    More Q

--- CMPUT291_MiniProject_1/src/project1/Queries.java
+++ CMPUT291_MiniProject_1/src/project1/Queries.java
@@ -204,33 +204,64 @@ public class Queries {
   						"' and src=' " + src + "'" +
   						"and dst='" + dst + "'));"; */
 
-return "select flightno, src, dst, dep_date, arr_time, stops, layover, price, seats" + 
-  		"from (" +
-	  		"select flightno, src, dst, dep_date, arr_time, stops, layover, price, seats" + 
-	 		"from" + 
-	  			"(select af.flightno1 (flightno), af.src, af.dst, af.dep_date, af.arr_time, 1 stops, gc.layover, af.price, af.seats" +
-	  			"from good_connections gc, available_flights af" +
-	  			"where gc.flightno1 = af.flightno and to_char(gc.dep_date,'DD/MM/YYYY')='" + 
-	  			dep_date + 
-	  			"' and gc.src='" + src +
-	  			"' and gc.dst='" + dst + "'" +
-			"union" +
-				"(select af.flightno2 (flightno), af.src, af.dst, af.dep_date, af.arr_time, 1 stops, gc.layover, af.price, af.seats" +
-	  			"from good_connections gc, available_flights af" +
-	  			"where gc.flightno2 = af.flightno and to_char(gc.dep_date,'DD/MM/YYYY')='" + 
-	  			dep_date + 
-	  			"' and gc.src='" + src +
-	  			"' and gc.dst='" + dst + "'" +
-			"union" +
-	  			"select flightno, src, dst, dep_date, arr_time, 0 stops, 0 layover, price, seats" +
-	  			"from available_flights" +
-	  			"where to_char(dep_date,'DD/MM/YYYY')='" +
-	  			dep_date + 
-	  			"' and src=' " + src + "'" +
-	  			"and dst='" + dst + "'));";
+		return "select flightno, src, dst, dep_date, arr_time, stops, layover, price, seats" + 
+		  		"from (" +
+			  		"select flightno, src, dst, dep_date, arr_time, stops, layover, price, seats" + 
+			 		"from" + 
+			  			"(select af.flightno1 (flightno), af.src, af.dst, af.dep_date, af.arr_time, 1 stops, gc.layover, af.price, af.seats" +
+			  			"from good_connections gc, available_flights af" +
+			  			"where gc.flightno1 = af.flightno and to_char(gc.dep_date,'DD/MM/YYYY')='" + 
+			  			dep_date + 
+			  			"' and gc.src='" + src +
+			  			"' and gc.dst='" + dst + "'" +
+					"union" +
+						"(select af.flightno2 (flightno), af.src, af.dst, af.dep_date, af.arr_time, 1 stops, gc.layover, af.price, af.seats" +
+			  			"from good_connections gc, available_flights af" +
+			  			"where gc.flightno2 = af.flightno and to_char(gc.dep_date,'DD/MM/YYYY')='" + 
+			  			dep_date + 
+			  			"' and gc.src='" + src +
+			  			"' and gc.dst='" + dst + "'" +
+					"union" +
+			  			"select flightno, src, dst, dep_date, arr_time, 0 stops, 0 layover, price, seats" +
+			  			"from available_flights" +
+			  			"where to_char(dep_date,'DD/MM/YYYY')='" +
+			  			dep_date + 
+			  			"' and src=' " + src + "'" +
+			  			"and dst='" + dst + "'))" +
+				"SORT BY price;" ;
 	}
-	
-	
-	
 
+
+	public static String checkPassengers(String email, String name){
+		//passengers(email, name, country) 
+		return "SELECT email, name" + 
+				"FROM passengers" +
+				"WHERE email = '" + email + "' and name = '" + name + "';"
+
+
+	}
+
+	public static String addPassenger(String email, String name, String country){
+
+		return "INSERT into passengers values(" +
+				email + " " name + " " + country + ");" ; 
+	}
+
+	public static String addBooking(String tno, String flightno, String fare, String dep_date, String seat){
+		//bookings(tno, flightno, fare, dep_date, seat)
+			return "INSERT into bookings values(" +
+				tno + " " flightno + " " + fare +
+				dep_date + " " seat + ");" ; 
+	}
+
+	public static String addTicket(String tno,String name,String email,String paid_price){
+		//tickets(tno, name, email, paid_price)
+			return "INSERT into tickets values(" +
+				tno + " " name + " " + email + paid_price ");" ; 
+	}
+
+
+	
+	
+	
 }
