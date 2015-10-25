791433e59af64bc92e931c4db661ac046db5cb69
quentinlautischer <lautisch@ualberta.ca>
2015-10-25 16:46:55 -0600

    more changed

--- CMPUT291_MiniProject_1/src/project1/SqlManager.java
+++ CMPUT291_MiniProject_1/src/project1/SqlManager.java
@@ -1,10 +1,7 @@
 package project1;
 
-import java.awt.List;
 import java.sql.ResultSet;
-import java.util.ArrayList;
 import java.util.HashMap;
-
 import textDevicePackage.TextDevice;
 
 public class SqlManager {
@@ -30,10 +27,40 @@ public class SqlManager {
 	}
 	
 	public void setup(){
-//		this.dropTables();
-//		this.createTables();
-//		this.insertData();
+		this.dropTables();
+		this.createTables();
+		this.insertData();
 	}
+	
+	public HashMap<String, String> searchFlights(String src, String dst, String dep_date){
+		// good_connections(src,dst,dep_date,flightno1,flightno2, layover,price)
+		//available_flights(flightno, dep_date, src , dst, dep_time, arr_time, fare, seats,price)
+		
+		HashMap<String, String> flights = new HashMap<String, String>();
+		String[] stmts = Queries.searchFlight(src, dst, dep_date, false, true);
+		String flightNo = "Oops";
+		String sourceAcode = "Oops";
+		String dstAcode = "Oops";
+		String dep_time = "Oops";
+		String arr_time = "Oops";
+		String numStops = "Oops";
+		String price = "Oops";
+		String seats = "Oops";
+
+		ResultSet rs = sqlDB.executeQuery(stmts[0]);
+			try {
+				while(rs.next()){
+//					flightNo = String.valueOf(rs.getString("flightno"));
+					
+					
+//					flights.put(flightNo, flightNo);
+				}
+			} catch (Exception e){
+				io.printf("availflights1 issue %s %n", e);
+			}
+		return flights;
+	}
+	
 	
 	public HashMap<String, String> getBookings(String email){
 		String tno;
@@ -120,6 +147,32 @@ public class SqlManager {
 	
 	public void insertData(){
 		queryWrapper(Queries.insertSampleData());
+	}
+	
+	public boolean checkValidSource(String src){
+		ResultSet rs = sqlDB.executeQuery(Queries.findAcode(src));
+		try{
+			if(!rs.isBeforeFirst()){
+				//Find with name
+				
+			} 
+			
+		} catch (Exception e){
+			io.printf("checkValidSource failed %s", e);
+		}
+		return true; 
+	}
+	
+	public boolean checkValidDestination(String dst){
+		ResultSet rs = sqlDB.executeQuery(Queries.findUser(dst));
+		try{
+			if(!rs.isBeforeFirst()){
+				return false;
+			}
+		} catch (Exception e){
+			io.printf("checkForUserWithEmail failed %s", e);
+		}
+		return true; 
 	}
 	
 	public boolean checkForUserWithEmail(String email){
