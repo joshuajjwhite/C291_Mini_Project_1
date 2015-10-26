efefd4ef6534db8943f25f5ffdba303fbc01db6d
Joshua White <jjwhite@cs.ualberta.ca>
2015-10-26 13:37:35 -0600

    Fixed Q

--- CMPUT291_MiniProject_1/src/project1/Queries.java
+++ CMPUT291_MiniProject_1/src/project1/Queries.java
@@ -363,7 +363,7 @@ public class Queries {
  				 "a2.dep_time-a1.arr_time, min(a1.price+a2.price), a1.seats || ', ' || a2.seats " +
  				 "from available_flights a1, available_flights a2 " +
  				 "where a1.dst=a2.src and a1.arr_time +1.5/24 <=a2.dep_time and a1.arr_time +5/24 >=a2.dep_time " +
- 				 "group by a1.src, a2.dst, a1.dep_date, min(a1.price+a2.price), a1.seats || ', ' || a2.seats, min(a1.price+a2.price), a1.flightno, a2.flightno, a2.dep_time, a1.arr_time "};
+ 				 "group by a1.src, a2.dst, a1.dep_date, a2.arr_time, a1.seats, a2.seats, a1.flightno, a2.flightno, a2.dep_time, a1.arr_time "};
 		 
 		 return gc;
 
@@ -383,7 +383,7 @@ public class Queries {
  				 "where a1.dst=a2.src and a2.dst = a3.src and " +
  				 " a1.arr_time +1.5/24 <=a2.dep_time and a1.arr_time +5/24 >=a2.dep_time " +
 
- 				 "group by a1.src, a2.dst, a1.dep_date, a1.flightno, a2.flightno, a2.dep_time, a1.arr_time "};
+ 				 "group by a1.src, a3.dst, a1.dep_date, a3.arr_time, a1.seats, a2.seats, a3.seats, a1.flightno, a2.flightno, a3.flightno, a3.dep_time, a2.arr_time, a2.dep_time, a1.arr_time "};
 		 
 		 return gc;
 
